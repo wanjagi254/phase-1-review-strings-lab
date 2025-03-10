6f814f77d6a236f9c7ec66d288fe9c1bf1b0cd73
brian <githinjibrian700@gmail.com>
2025-03-10 11:42:03 +0300

    initial text

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const myString = 'template literal';
+
+const myNumber = 10;
+
+const myBoolean = false;
+
+`Saying that interpolation with ${myString}s is better than concatenation ${90 + myNumber}% of the time is simply ${myBoolean}. But it is pretty cool!
+
+Beware that new lines inside of a ${myString} will be preserved as new lines in the resulting ${typeof myString}!`;
