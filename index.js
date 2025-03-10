b0af470f43416856c94cf9e7d133d69c83713184
brian <githinjibrian700@gmail.com>
2025-03-10 11:45:32 +0300

    change text

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const myString = 'template literal';
+const currentUser = 'Grace Hopper';
 
-const myNumber = 10;
+...
 
-const myBoolean = false;
-
-`Saying that interpolation with ${myString}s is better than concatenation ${90 + myNumber}% of the time is simply ${myBoolean}. But it is pretty cool!
-
-Beware that new lines inside of a ${myString} will be preserved as new lines in the resulting ${typeof myString}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
